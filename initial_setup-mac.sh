e6155cd1ab8bc4f9fb3984a452151dea08d00c21
Dan Jones <deltajuliet@users.noreply.github.com>
2021-06-21 21:28:33 -0600

    Adds Mac detection logic

--- initial_setup-mac.sh
+++ initial_setup-mac.sh
@@ -5,6 +5,14 @@
 #
 # Created by Dan Jones
 
+unamestr=`uname`
+if [[ "$unamestr" == 'Darwin' ]]; then
+  echo "[-] Mac detected, proceeding with install"
+else
+  echo "[!] This doesn't appear to a Mac! Aborting!"
+  exit 1
+fi
+
 which brew
 if [[ $? != 0 ]] ; then
     # Install Homebrew
