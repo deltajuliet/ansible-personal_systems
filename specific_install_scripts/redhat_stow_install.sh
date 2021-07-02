6d368287e9561dcfd01582f5d761a0975404e226
Dan Jones <1455270+deltajuliet@users.noreply.github.com>
2021-07-02 15:38:04 -0600

    Actually fixes the path issues
    Signed-off-by: Dan Jones <1455270+deltajuliet@users.noreply.github.com>

--- specific_install_scripts/redhat_stow_install.sh
+++ specific_install_scripts/redhat_stow_install.sh
@@ -7,6 +7,6 @@ fi
 yum -y install perl-Test-Simple perl-Test-Output # Install stow dependencies
 mkdir /tmp/stow-latest
 cd /tmp/stow-latest
-curl -o /stow-latest.tar.gz http://ftp.gnu.org/gnu/stow/stow-latest.tar.gz
+curl -o stow-latest.tar.gz http://ftp.gnu.org/gnu/stow/stow-latest.tar.gz
 tar xzf stow-latest.tar.gz --strip-components 1
 ./configure && make && make install
