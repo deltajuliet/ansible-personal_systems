4ca77c5951d39a0e96bc26f629690e54da1a8648
Dan Jones <1455270+deltajuliet@users.noreply.github.com>
2021-07-02 15:05:45 -0600

    Removes sudo for systems that don't use it like AL2
    Signed-off-by: Dan Jones <1455270+deltajuliet@users.noreply.github.com>

--- specific_install_scripts/redhat_stow_install.sh
+++ specific_install_scripts/redhat_stow_install.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-sudo yum install perl-Test-Simple perl-Test-Output # Install stow dependencies
+who = whoami
+
+if [[ $who != "root"]]; then
+    echo "This script needs to be run as root"
+    exit 1
+fi
+
+yum install perl-Test-Simple perl-Test-Output # Install stow dependencies
 curl -o /tmp/stow-latest.tar.gz http://ftp.gnu.org/gnu/stow/stow-latest.tar.gz
 tar xzf /tmp/stow-latest.tar.gz
 cd /tmp/stow-latest
-./configure && make && sudo make install
+./configure && make && make install
