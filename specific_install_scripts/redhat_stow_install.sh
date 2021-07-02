291fd1a40576cd19de5f22d58f202bc86128422f
Dan Jones <1455270+deltajuliet@users.noreply.github.com>
2021-07-02 15:16:14 -0600

    Needs to move into dir with the tarball
    Signed-off-by: Dan Jones <1455270+deltajuliet@users.noreply.github.com>

--- specific_install_scripts/redhat_stow_install.sh
+++ specific_install_scripts/redhat_stow_install.sh
@@ -6,6 +6,7 @@ fi
 
 yum -y install perl-Test-Simple perl-Test-Output # Install stow dependencies
 curl -o /tmp/stow-latest.tar.gz http://ftp.gnu.org/gnu/stow/stow-latest.tar.gz
+cd /temp
 tar xzf /tmp/stow-latest.tar.gz
 cd /tmp/stow-latest
 ./configure && make && make install
