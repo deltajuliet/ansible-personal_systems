a8410d0fa65729c111d2927c66e4ad057b7b1608
Dan Jones <dan.jones@liveviewtech.com>
2022-10-12 10:10:19 -0400

    Fixes variable bugs
    Signed-off-by: Dan Jones <dan.jones@liveviewtech.com>

--- specific_install_scripts/change_repos_to_ssh.sh
+++ specific_install_scripts/change_repos_to_ssh.sh
@@ -1,18 +1,18 @@
-var repo_path=/tmp
-var repo_ssh_url=none
+repo_path=/tmp
+repo_ssh_url=none
 
 function http_to_ssh_git () {
     if [[ -d $repo_path ]]; then
         echo "[!] Switching " + $repo_path + " to ssh"
-        cd @repo_path
+        cd $repo_path
         git remote set-url origin $repo_ssh_url
     fi
 }
 
-var repo_path=~/code/github/dotfiles
-var repo_ssh_url="git@github.com:deltajuliet/dotfiles.git"
+repo_path=~/code/github/dotfiles
+repo_ssh_url="git@github.com:deltajuliet/dotfiles.git"
 http_to_ssh_git()
 
-var repo_path=~/code/github/ansible-personal_systems
-var repo_ssh_url="git@github.com:deltajuliet/ansible-personal_systems.git"
+repo_path=~/code/github/ansible-personal_systems
+repo_ssh_url="git@github.com:deltajuliet/ansible-personal_systems.git"
 http_to_ssh_git()
